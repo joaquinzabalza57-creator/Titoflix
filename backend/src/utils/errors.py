7a2d39d390631203ffd1b15b5ab9e7b8015d4aef
joaquinzabalza57-creator <joaquin.zabalza@philips.edu.ar>
2026-05-06 11:30:42 -0300

    Agregar errores posibles

--- backend/src/utils/errors.py
+++ backend/src/utils/errors.py
@@ -5,12 +5,13 @@ class AppError(Exception):
     def __init__(self, message: str | None = None):
         if message:
             self.message = message
+
         super().__init__(self.message)
 
 
-class NotFoundError(AppError):
-    status_code = 404
-    message = "Resource not found"
+class BadRequestError(AppError):
+    status_code = 400
+    message = "Bad request"
 
 
 class UnauthorizedError(AppError):
@@ -23,6 +24,16 @@ class ForbiddenError(AppError):
     message = "Forbidden"
 
 
+class NotFoundError(AppError):
+    status_code = 404
+    message = "Resource not found"
+
+
 class ConflictError(AppError):
     status_code = 409
     message = "Conflict"
+
+
+class ValidationError(AppError):
+    status_code = 422
+    message = "Validation error"
