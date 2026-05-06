578871572731ddc7475b1853916d45e0b8e25826
joaquinzabalza57-creator <joaquin.zabalza@philips.edu.ar>
2026-05-06 11:26:15 -0300

    Agrear imports y exports de servicios

---
+++ backend/src/services/__init__.py
@@ -0,0 +1,24 @@
+from src.services.auth_service import AuthService
+from src.services.product_service import (
+    CalificacionService,
+    ContenidoService,
+    EpisodioService,
+    GeneroService,
+    MiListaService,
+    TemporadaService,
+    VistaService,
+)
+from src.services.user_service import CuentaService, PerfilService
+
+__all__ = [
+    "AuthService",
+    "CuentaService",
+    "PerfilService",
+    "GeneroService",
+    "ContenidoService",
+    "TemporadaService",
+    "EpisodioService",
+    "VistaService",
+    "MiListaService",
+    "CalificacionService",
+]
